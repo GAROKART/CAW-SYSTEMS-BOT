85b9645e39edbe9eed66fd367575a8467d365ea9
Dinkelberg <dinkelberg-norepl@gmx.net>
2022-01-28 04:19:14 +0100

    initialize github repository

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const generateImage = require("./generateImage")
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi"){
+        message.reply("Hello World!")
+    }
+})
+
+const welcomeChannelId = "936415775923777566"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Warum bist du hier der server ist voll scheiße`,
+        files: [img]
+    })
+})
+
+client.login(process.env.TOKEN)
